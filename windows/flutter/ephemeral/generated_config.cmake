d21102cbefbed5affed0839ccfd3e76ec02ce280
hafisc <alhafiscloud@gmail.com>
2025-06-28 00:30:46 +0700

    Initial commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Dapur Anita\\Dapur anita app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Src\\flutter"
+  "PROJECT_DIR=E:\\Dapur Anita\\Dapur anita app"
+  "FLUTTER_ROOT=C:\\Src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Dapur Anita\\Dapur anita app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Dapur Anita\\Dapur anita app"
+  "FLUTTER_TARGET=E:\\Dapur Anita\\Dapur anita app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YjI1MzA1YTg4Mw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTQyNWU1ZTllYw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Dapur Anita\\Dapur anita app\\.dart_tool\\package_config.json"
+)
